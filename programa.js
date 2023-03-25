795d89e273866e316660f31a71fc054378ce3688
scrodoge <nevillamizar@uts.edu.co>
2023-03-25 16:42:08 -0500

    Primer commit

---
+++ programa.js
@@ -0,0 +1,29 @@
+const prompt = require("prompt-sync")()
+
+console.log("¡Bienvenido!");
+let N = prompt ('¿Cual es tu nombre?');
+
+console.log(`Bienvenid@ ${N} a MarkoRegalos`);
+/**
+ * Hola
+ * 
+ * Neider
+ */
+function seleccion () {
+    let ct = prompt ('¿que catalogo deseas consultar?');r//preguntar,ct=catalogo
+    let confirm = prompt (`deseas catalogo de ${ct} es correcto?`) ;
+    if (confirm == "Si") {
+        console.log(`El catalogo seleccionado fue: ${ct}`)
+        return;
+    } else {
+        seleccion()
+    }
+}
+
+seleccion();
+
+console.log()
+
+
+
+
